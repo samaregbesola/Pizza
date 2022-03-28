d71b1f382ed58186ae2f44c48141910b09bf7866
samaregbesola <samuelaregbesola@gmail.com>
2022-03-28 23:39:22 +0100

    netlify hosting and redirect fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { motion } from 'framer-motion';
-import Loader from './Loader';
+import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import Loader from "./Loader";
 
 const buttonVariants = {
   hover: {
     scale: 1.1,
-    textShadow: '0px 0px 8px rgb(255, 255, 255)',
-    boxShadow: '0px 0px 8px rgb(255, 255, 255',
+    textShadow: "0px 0px 8px rgb(255, 255, 255)",
+    boxShadow: "0px 0px 8px rgb(255, 255, 255",
     transition: {
       duration: 0.3,
       repeat: Infinity,
-      repeatType: 'reverse',
+      repeatType: "reverse",
     },
   },
 };
@@ -28,8 +28,8 @@ const containerVariants = {
     },
   },
   exit: {
-    x: '-100vw',
-    transition: { ease: 'easeInOut' },
+    x: "-100vw",
+    transition: { ease: "easeInOut" },
   },
 };
 
@@ -45,7 +45,7 @@ const Home = () => {
       <h2>Welcome to Pizza Joint</h2>
       <Link to="/base">
         <motion.button variants={buttonVariants} whileHover="hover">
-          Create Your Pizza
+          Welcome to Pizza & Toppings
         </motion.button>
       </Link>
       <Loader />
